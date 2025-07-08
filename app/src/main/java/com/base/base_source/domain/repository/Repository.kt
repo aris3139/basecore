1f16feb8e351d0e3044f9a20516512724b0f7ff0
KienFpt <kienmt6@fpt.com>
2025-07-08 15:13:56 +0700

    migrate Clean Architecture Success

--- app/src/main/java/com/base/base_source/domain/repository/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.base.base_source.domain.repository
-
-/**
- * Base interface for all repositories in the domain layer.
- * This is a marker interface to identify repository components.
- */
-interface Repository
