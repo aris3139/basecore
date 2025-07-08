1ef849586805912f7d878f39f4ab75039ff6f555
KienFpt <kienmt6@fpt.com>
2025-07-08 13:23:47 +0700

    migration

---
+++ app/src/main/java/com/base/base_source/domain/repository/Repository.kt
@@ -0,0 +1,7 @@
+package com.base.base_source.domain.repository
+
+/**
+ * Base interface for all repositories in the domain layer.
+ * This is a marker interface to identify repository components.
+ */
+interface Repository
