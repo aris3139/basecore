da2a180f6c737f239ccd02dc90f988a58e9badcf
KienFpt <kienmt6@fpt.com>
2025-07-09 16:51:50 +0700

    init header view

--- app/src/main/java/com/base/base_source/ui/home/HomeScreen.kt
+++ app/src/main/java/com/base/base_source/ui/home/HomeScreen.kt
@@ -1,19 +1,21 @@
 package com.base.base_source.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import com.base.base_source.navigation.FeedDetail
+import com.base.base_source.ui.home.component.Header
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,40 +24,24 @@ fun HomeScreen(
     onNavigateToFeedDetail: (FeedDetail) -> Unit,
     onNavigateToSettings: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TopAppBar(
-            title = { Text("Home") }
-        )
+    Scaffold(
+        topBar = {
+            Header(
+                modifier = Modifier.padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
 
-        Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Button(
-            onClick = {
-                onNavigateToFeedDetail(
-                    FeedDetail(
-                        feedId = "feed123",
-                    )
-                )
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Go to Product Detail")
+            }
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+    )
 
-        Button(
-            onClick = { onNavigateToSettings() },
-            modifier = Modifier.fillMaxWidth()
-
-        ) {
-            Text("Go to Settings")
-        }
-    }
 }
---
+++ app/src/main/java/com/base/base_source/ui/home/component/Header.kt
@@ -0,0 +1,79 @@
+package com.base.base_source.ui.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.base.base_source.R
+
+
+@Composable
+fun Header(
+    modifier: Modifier = Modifier,
+    onCameraClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        IconButton(
+            onClick = { onCameraClick() },
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_camera),
+                contentDescription = "Camera Icon",
+                modifier = Modifier
+                    .size(32.dp)
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_home_insta),
+            contentDescription = "Camera Icon",
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification),
+                    contentDescription = "Notification Icon",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(2.dp))
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_messanger),
+                    contentDescription = "Messenger Icon",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HeaderPreview() {
+    Header()
+}
