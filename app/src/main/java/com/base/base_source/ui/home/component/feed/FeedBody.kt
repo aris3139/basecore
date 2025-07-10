ab3447eec6261b80ba4d3ffc1416ec25d0aae68d
KienFpt <kienmt6@fpt.com>
2025-07-10 15:24:21 +0700

    shuffle img

--- app/src/main/java/com/base/base_source/ui/home/component/feed/FeedBody.kt
+++ app/src/main/java/com/base/base_source/ui/home/component/feed/FeedBody.kt
@@ -2,6 +2,7 @@ package com.base.base_source.ui.home.component.feed
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -13,10 +14,23 @@ import com.base.base_source.R
 fun FeedBody(
     modifier: Modifier = Modifier,
 ) {
+    val images = listOf(
+        R.drawable.thumbnail_1,
+        R.drawable.thumbnai_2,
+        R.drawable.thumbnai_4,
+        R.drawable.thumbnai_5,
+        R.drawable.thumbnai_3,
+        R.drawable.thumbnai_6,
+        R.drawable.thumbnai_7,
+        R.drawable.thumbnai_8,
+    )
+    val imgId = remember { images.random() }
+
+
     Image(
         modifier = modifier,
         contentDescription = "Feed Body",
-        painter = painterResource(id = R.drawable.thumbnail_1),
+        painter = painterResource(id = imgId),
         contentScale = ContentScale.FillWidth
 
     )
